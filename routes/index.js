b8f744fe21b0fe82da2a9d031af6e993ecf0c91b
type0 <ghyghygg30@gmail.com>
2016-03-20 07:21:38 +0000

    some added

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,19 @@ var freq = _.groupBy(require("../Frequency.json"),'pid');
 var client = es.Client({
   host: "192.168.0.19:9200"
 });
-var bllist;
+var bllist=[],linklist=[];
 csv.fromPath('Allword.csv')
   .on('data', function(data){
-    bllist = _.map(data,function(row){return row[1];});
+    bllist.push(data[1]);
   })
-
+csv.fromPath('Patent_link.csv')
+  .on('data', function(data){
+    linklist.push(data[1]);
+  })
 module.exports = function(app){
   app.get('/get_stat', function(req, res){
     var q = req.query.q;
+    var re = /(\s|\.|\n)+$/i;
     if(!q)return res.json([]);
     var terms = q;
     client.search({q: terms}, function(err, resp){
@@ -22,11 +26,16 @@ module.exports = function(app){
       var hits = resp.hits.hits;
       var stat = [];
       _.each(hits, function(hit){
-        state.push({
-
-        })
+        var date_obj = moment(hit._source.priorityDate,"MMM DD, YYYY");
+        //var des_terms = _.compact(hit._source.description.replace(re," ").split(" "));
+        stat.push({
+          year:date_obj.year(),
+          doc: hit
+        });
       });
-     return res.json(stat)
+      var yearGroup = _.groupBy(stat, 'year');
+      console.log(yearGroup)
+      return res.json(hits)
     });
   })
   app.get('/get_data', function(req, res){
@@ -36,21 +45,24 @@ module.exports = function(app){
     client.search({q: terms}, function(err, resp){
       if(err){console.log(err);return res.status(500).json({error: "Error occured"});}
       var hits = resp.hits.hits;
-      terms = [];
+      terms = [], l =[];
       _.each(hits, function(hit){
         var id = parseInt(hit._id) + 428;
         if(freq[id]){
-          terms = terms.concat(freq[id]);
+          terms = _.map(_.reject(terms.concat(freq[id]),function(term){return q.split(",").indexOf(term.word) != -1;}),function(t){t.frequency=t.frequency+hit.score;return t;});
+	  l.push({link:linklist[id],name:hit._source.name});
 	}
+        
       });
       terms.sort(function(a,b){
         return b.frequency - a.frequency;
       });
-      terms.splice(8);
+      terms.splice(7);
       data = _.map(terms,function(row){
         return {
           term: row.word,
-          score: row.frequency * 1000
+          score: row.frequency * 1000,
+	  link:l 
         };
       })
       res.set('Access-Control-Allow-Origin','*')
